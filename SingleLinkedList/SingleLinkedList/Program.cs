e79e4b5b692074c2dfd44e30d08eafdc99b7cad2
emanul <emnaueljose0522@gmail.com>
2026-01-19 15:16:12 -0500

    single linked list

---
+++ SingleLinkedList/SingleLinkedList/Program.cs
@@ -0,0 +1,67 @@
+﻿
+//make a generic node class
+public class Node<T> 
+{
+    public T Data;
+    public Node<T> Next;
+
+    public Node(T data )
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+
+//make a generic single linked list class
+public class SingleLinkedList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+    public SingleLinkedList()
+    {
+        head = null;
+        tail = null;
+    }
+
+    public void Add(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void PrintList()
+    {
+        Node<T> curr = head;
+
+        while(curr != null)
+        {
+            Console.Write(curr.Data + " -> ");
+            curr = curr.Next;
+        }
+        Console.Write("null");
+    }
+}
+
+//main program
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SingleLinkedList<int> list = new SingleLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.PrintList();
+
+    }
+}
